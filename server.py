50f770dd6538de947ac44e70e1248b8c5d58f896
Jari Peltola <jena9988@gmail.com>
2025-02-12 19:53:13 +0200

    styling

--- server.py
+++ server.py
@@ -40,5 +40,5 @@ def weather():
 
 if __name__ == "__main__":
     from waitress import serve
-    port = int(os.environ.get("PORT", 8080))  # Render tarvitsee porttiasetuksen
+    port = int(os.environ.get("PORT", 8080))  
     serve(app, host="0.0.0.0", port=port)
